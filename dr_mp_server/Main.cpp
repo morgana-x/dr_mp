95320c9c043a28c96528f9c5743263dee03e610e
morgana-x <89588301+morgana-x@users.noreply.github.com>
2026-05-20 23:41:29 +1000

    Add port argument to server

--- dr_mp_server/Main.cpp
+++ dr_mp_server/Main.cpp
@@ -1,9 +1,11 @@
-// dr_mp_server.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "Server.hpp"
 #include <thread>
+#include <stdio.h>
+#include <stdlib.h>
+extern "C" {
+#include "args.h"
+}
 
 Server server;
 
@@ -25,9 +27,28 @@ void close_server()
     server.Close();
 }
 
-int main()
+int main(int argc, char** argv)
 {
     int port = 64355;
+
+    // Instantiate a new ArgParser instance.
+    ArgParser* parser = ap_new_parser();
+    if (!parser) {
+        exit(1);
+    }
+
+    // Register the program's helptext and version number.
+    ap_set_helptext(parser, "Usage: dr_mp_server.exe -p 64355");
+    ap_set_version(parser, "1.0");
+
+    ap_add_int_opt(parser, "port p", port);
+
+    // Parse the command line arguments.
+    if (!ap_parse(parser, argc, argv)) {
+        exit(1);
+    }
+
+    port = ap_get_int_value(parser, "port");
 
     std::cout << "Starting server using port " << port << "...\n";
 
